ad8662689aa620fba6e8bbdc53c49cee330137f1
vetriselvan2004 <selvanvetri448@gmail.com>
2025-03-01 23:04:09 +0530

    added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,9 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOW_CREDENTIALS = True
 
