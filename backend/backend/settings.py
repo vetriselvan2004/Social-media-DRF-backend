976bb224ac24138e690d3c544f5b3cc25a4a0d69
vetriselvan2004 <selvanvetri448@gmail.com>
2025-03-01 23:15:56 +0530

    Added Render domain to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,7 +70,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", 
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
